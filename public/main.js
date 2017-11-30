dca0cc039ed55f606b08667ff70b2de020ca2d51
John Murphy <murf2073@gmail.com>
2017-11-30 15:17:49 -0500

    whitespace, formatting, expanded wordlist

--- public/main.js
+++ public/main.js
@@ -1,169 +1,185 @@
 var socket = io();
-var player;
+var user;
+
+function usernameAsk() {
+    $('.grey-out').fadeIn(500);
+    $('.user').fadeIn(500);
+    $('.user').submit(function(){
+        event.preventDefault();
+        user = $('#username').val().trim();
+
+        if (user == '') {
+            return false
+        };
+
+        var index = users.indexOf(user);
+
+        if (index > -1) {
+            alert(user + ' already exists');
+            return false
+        };
+        
+        socket.emit('join', user);
+        $('.grey-out').fadeOut(300);
+        $('.user').fadeOut(300);
+        $('input.guess-input').focus();
+    });
+};
+
 var context;
 var canvas;
 var click = false;
-var players = [];
 
-function usernameAsk() {
-	$('.grey-out').fadeIn(300);
-	$('.user').fadeIn(300);
-	$('.user').submit(function(e){
-		e.preventDefault();
-		player = $('.username').val().trim();
-
-		if (player == ''){
-			return false
-		};
-
-		var index = players.indexOf(player);
-
-		if (index > -1){
-			alert(player + ' already exists');
-			return false
-		};
-
-		socket.emit('join', player);
-		$('.grey-out').fadeOut(200);
-		$('.user').fadeOut(200);
-		$('input.guess-input').focus();
-	});
+var clearScreen = function() {
+    context.clearRect(0, 0, canvas[0].width, canvas[0].height);
 };
 
-var clearScreen = function(){
-	context.clearRect(0, 0, canvas[0].width, canvas[0].height);
-};
+var guesser = function() {
+    clearScreen();
+    click = false;
+    console.log('draw status: ' + click);
+    $('.draw').hide();
+    $('#guesses').empty();
+    console.log('You are a guesser');
+    $('#guess').show();
+    $('.guess-input').focus();
 
-var guesser = function(){
-	clearScreen();
-	click = false;
-	$('#guesses').empty();
-	$('.draw').hide();
-	$('#guess').show();
-	$('.guess-input').focus();
+    $('#guess').on('submit', function() {
+        event.preventDefault();
+        var guess = $('.guess-input').val();
 
-	$('#guess').on('submit', function(e){
-		e.preventDefault();
-		var guess = $('.guess-input').val();
+        if (guess == '') {
+            return false
+        };
 
-		if(guess == ''){
-			return false
-		};
-
-		socket.emit('guessword', {username: player, guessword: guess});
-		$('.guess-input').val('');
-	});
+        console.log(user + "'s guess: " + guess);
+        socket.emit('guessword', {username: user, guessword: guess});
+        $('.guess-input').val('');
+    });
 };
 
 var guessword = function(data){
-	$('#guesses').text(data.username + "'s guess: " + data.guessword);
+    $('#guesses').text(data.username + "'s guess: " + data.guessword);
 
-	if(click == true && data.guessword == $('span.word').text()){
-		socket.emit('correct answer', {username: data.username, guessword: data.guessword}):
-		socket.emit('swap rooms', {from: player, to: data.username});
-		click = false;
-	};
+    if (click == true && data.guessword == $('span.word').text() ) {
+        console.log('guesser: ' + data.username + ' draw-word: ' + $('span.word').text());
+        socket.emit('correct answer', {username: data.username, guessword: data.guessword});
+        socket.emit('swap rooms', {from: user, to: data.username});
+        click = false;
+    }
 };
 
-var drawWord = function(word){
-	$('span.word').text(word);
+var drawWord = function(word) {
+    $('span.word').text(word);
+    console.log('Your word to draw is: ' + word);
 };
 
-var userlist = function(names){
-	players = names;
-	var html = '<p class="chatbox-header">' + 'Players' + '</p>';
-	for (var i = 0; i < names.length; i++){
-		html += '<li>' + names[i] + '</li>';
-	};
-	$('ul').html(html);
+var users = [];
+
+var userlist = function(names) {
+    users = names;
+    var html = '<p class="chatbox-header">' + 'Players' + '</p>';
+    for (var i = 0; i < names.length; i++) {
+        html += '<li>' + names[i] + '</li>';
+    };
+    $('ul').html(html);
 };
 
 var newDrawer = function() {
-	socket.emit('new drawer', player);
-	clearScreen();
-	$('#guesses').empty();
+    socket.emit('new drawer', user);
+    clearScreen();
+    $('#guesses').empty();
 };
 
-var correctAnswer = function(data){
-	$('#guesses').html('<p>' + data.username + ' guessed correctly!' + '</p>');	
+var correctAnswer = function(data) {
+    $('#guesses').html('<p>' + data.username + ' guessed correctly!' + '</p>');
 };
 
-var reset = function(name){
-	clearScreen();
-	$('#guesses').empty();
-	$('#guesses').html('<p>' + name + ' is the new drawer' + '</p>');
+var reset = function(name) {
+    clearScreen();
+    $('#guesses').empty();
+    console.log('New drawer: ' + name);
+    $('#guesses').html('<p>' + name + ' is the new drawer' + '</p>');
 };
 
-var draw = function(obj){
-	context.fillStyle = obj.color;
-	context.beginPath();
-	context.arc(obj.postion.x, obj.postion.y, 3, 0, 2 * Math.PI);
-	context.fill();
+var draw = function(obj) {
+    context.fillStyle = obj.color;
+    context.beginPath();
+    context.arc(obj.position.x, obj.position.y,
+                     3, 0, 2 * Math.PI);
+    context.fill();
 };
 
-var pictionary = function(){
-	clearScreen();
-	click = true;
-	$('#guess').hide();
-	$('#guesses').empty();
-	$('.draw').show();
+var pictionary = function() {
+    clearScreen();
+    click = true;
+    console.log('draw status: ' + click);
+    $('#guess').hide();
+    $('#guesses').empty();
+    $('.draw').show();
 
-	var drawing;
-	var color;
-	var obj = {};
+    var drawing;
+    var color;
+    var obj = {};
 
-	$('.draw-buttons').on('click', 'button', function(){
-		obj.color = $(this).attr('value');\
+    $('.draw-buttons').on('click', 'button', function(){
+        obj.color = $(this).attr('value');
+        console.log(obj.color);
 
-		if(obj.color === '0'){
-			socket.emit('clear screen', player);
-			context.fillStyle = 'white';
-			return;
-		};
-	});
+        if (obj.color === '0') {
+            socket.emit('clear screen', user);
+            context.fillStyle = 'white';
+            return;
+        };
+    });
 
-	$('.users').on('dblclick', 'li', function(){
-		if(click == true){
-			var target = $(this).text();
-			socket.emit('swap rooms', {from: player, to: target});
-		};
-	});
+    console.log('You are the drawer');
 
-	canvas.on('mousedown', function(event){
-		drawing = true;
-	});
+    $('.users').on('dblclick', 'li', function() {
+        if (click == true) {
+            var target = $(this).text();
+            socket.emit('swap rooms', {from: user, to: target});
+        };
+    });
 
-	canvas.on('mouseup', function(event){
-		drawing = false;
-	});
+    canvas.on('mousedown', function(event) { 
+        drawing = true;   
+    });
+    canvas.on('mouseup', function(event) {
+        drawing = false;
+    });
 
-	canvas.on('mousemove', function(event){
-		var offset = canvas.offset();
-		obj.position = {x: event.pageX - offset.left, y: event.pageY = offset.top};
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+        obj.position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        
+        if (drawing == true && click == true) {
+            draw(obj);
+            socket.emit('draw', obj);
+        };
+    });
 
-		if(drawing == true && click == true){
-			draw(obj);
-			socket.emit('draw', obj);
-		};
-	});
 };
 
-$(document).ready(function(){
-	canvas = $('#canvas');
-	context = canvas[0].getContext('2d');
-	canvas[0].width = canvas[0].offsetWidth;
-	canvas[0].height = canvas[0].offsetHeight;
+$(document).ready(function() {
 
-	usernameAsk();
+    canvas = $('#canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
 
-	socket.on('userlist', userlist);
-	socket.on('guesser', guesser);
-	socket.on('guessword', guessword);
-	socket.on('draw', draw);
-	socket.on('draw word', drawWord);
-	socket.on('drawer', pictionary);
-	socket.on('new drawer', newDrawer);
-	socket.on('correct answer', correctAnswer);
-	socket.on('reset', reset);
-	socket.on('clear screen', clearScreen);
+    usernameAsk();
+
+    socket.on('userlist', userlist);
+    socket.on('guesser', guesser);
+    socket.on('guessword', guessword);
+    socket.on('draw', draw);
+    socket.on('draw word', drawWord);
+    socket.on('drawer', pictionary);
+    socket.on('new drawer', newDrawer);
+    socket.on('correct answer', correctAnswer);
+    socket.on('reset', reset);
+    socket.on('clear screen', clearScreen);
+
 });
